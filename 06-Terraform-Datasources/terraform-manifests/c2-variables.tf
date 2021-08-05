289c9573a9c34b9a72a23b4acd07f54a162d42f6
Dale Wickizer <dwickizer@mitre.org>
2021-08-05 13:30:00 -0400

    Modified to comply with MITRE GovCloud policies + added my ami.

--- 06-Terraform-Datasources/terraform-manifests/c2-variables.tf
+++ 06-Terraform-Datasources/terraform-manifests/c2-variables.tf
@@ -1,22 +1,12 @@
 # Input Variables
 variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-  default = "us-east-1"
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "us-gov-west-1"
 }
-
-/* 
-# Commented as we are going to get AMI ID from Datasource aws_ami
-variable "ec2_ami_id" {
-  description = "AMI ID"
-  type = string  
-  default = "ami-0915bcb5fa77e4892"
-}
-*/
 
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
   type = string
   default = "t3.micro"
 }
-
