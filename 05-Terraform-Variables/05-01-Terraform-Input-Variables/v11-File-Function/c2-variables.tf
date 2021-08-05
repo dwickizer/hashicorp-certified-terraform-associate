6ee78ecfc97302bbad6c753e36bc2b64cb710ddd
Dale Wickizer <dwickizer@mitre.org>
2021-08-04 20:35:15 -0400

    Modified to comply with MITRE GovCloud policies.

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v11-File-Function/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v11-File-Function/c2-variables.tf
@@ -1,20 +1,20 @@
 # Input Variables
 variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-  default = "us-east-1"
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "us-gov-west-1"
 }
 
 variable "ec2_ami_id" {
   description = "AMI ID"
-  type = string  
-  default = "ami-0915bcb5fa77e4892"
+  type        = string
+  default     = "ami-0645cf757ebd107e5" # Amazon2 Linux AMI ID
 }
 
 variable "ec2_instance_count" {
   description = "EC2 Instance Count"
-  type = number
-  default = 1
+  type        = number
+  default     = 2
 }
 
 variable "ec2_instance_type" {
---
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v11-File-Function/c5-ec2-instance.tf
@@ -0,0 +1,15 @@
+# Create EC2 Instance
+resource "aws_instance" "my-ec2-vm" {
+  ami = var.ec2_ami_id
+  instance_type = var.ec2_instance_type
+  key_name = "terraform-key"
+  subnet_id = aws_subnet.vpc-tf-public-subnet-1.id
+  # subnet_id = aws_subnet.vpc-tf-public-subnet-2.id
+  count = var.ec2_instance_count
+  user_data = file("apache-install.sh")
+  vpc_security_group_ids = [aws_security_group.vpc-tf-ssh.id, aws_security_group.vpc-tf-web.id]
+  tags = {
+    "Name" = "webvars4-${count.index}"
+    "Owner" = "dwickizer"
+  }
+}
