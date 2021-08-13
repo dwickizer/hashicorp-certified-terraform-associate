76299b289b68b35939964038eb8dbaaf03b65137
Dale Wickizer <dwickizer@mitre.org>
2021-08-12 23:28:05 -0400

    Updated script for user data.

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v3-Input-Variables-Override-default-with-cli-var/apache-install.sh
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v3-Input-Variables-Override-default-with-cli-var/apache-install.sh
@@ -4,4 +4,5 @@ sudo yum install -y httpd
 sudo service httpd start  
 sudo systemctl enable httpd
 sudo echo "<h1>Hostname: $HOSTNAME</h1>" > /var/www/html/index.html
-sudo echo "<h1>Welcome to C2DM created using Terraform in us-gov-west-1 Region</h1>" >> /var/www/html/index.html
+REGION=`echo $HOSTNAME | cut -d. -f2`
+sudo echo "<h1>Welcome to C2DM created using Terraform in $REGION Region</h1>" >> /var/www/html/index.html
